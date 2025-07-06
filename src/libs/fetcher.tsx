5b7c8f3b6e5a24c09355448047c7fbde6a123e9d
bossnare <razafimangagervaischristo@gmail.com>
2025-07-06 15:51:54 +0300

    notes: add brain vite and tsconfig

--- src/libs/fetcher.tsx
+++ src/libs/fetcher.tsx
@@ -1,7 +1,5 @@
-import type { FetchProps } from '@/types/propTypes';
-
-export const fetcher = async ({ url }: FetchProps) => {
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
+export const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
 };
