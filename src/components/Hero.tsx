f9d3997f5c2926e389ec0aff58edc1f47c412d01
bossnare <razafimangagervaischristo@gmail.com>
2025-07-06 16:33:08 +0300

    feat: refactor Main component and add Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+import { fetcher } from '@/libs/fetcher';
+import { API_KEY } from '@/utils/constants';
+import Button from './ui/Button';
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="prose bg-gray-100 rounded-sm p-2">
+        <h1>BONJOUR</h1>
+        <p>Hello! My name is Jump Jump to quick haha...</p>
+        <Button
+          onClick={async () => {
+            const data = await fetcher(
+              `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Antananarivo&aqi=no`
+            );
+            console.log(data);
+          }}
+          className={`bg-blue-700 text-white`}
+          label={'Button'}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,9 @@
-import { fetcher } from '@/libs/fetcher';
-import { API_KEY } from '@/utils/constants';
-import Button from './ui/Button';
+import Hero from './Hero';
 
 const Main = () => {
   return (
     <main className="container">
-      <div className="prose bg-gray-100 rounded-sm p-1">
-        <h1>BONJOUR</h1>
-        <p>Hello! My name is Jump Jump to quick haha...</p>
-        <Button
-          onClick={async () => {
-            const data = await fetcher(
-              `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Antananarivo&aqi=no`
-            );
-            console.log(data);
-          }}
-          className={`bg-blue-700 text-white`}
-          label={'Button'}
-        />
-      </div>
+      <Hero />
     </main>
   );
 };
