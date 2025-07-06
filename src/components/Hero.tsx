b0826188fe53f9159fa86c66a94a917a1178f94f
bossnare <razafimangagervaischristo@gmail.com>
2025-07-06 20:34:03 +0300

    ref: refactor Hero component to improve weather data handling and update propTypes for WeatherData

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,32 +3,26 @@ import { API_URL } from '@/utils/constants';
 import Button from './ui/Button';
 import { useState } from 'react';
 import randomCity from '@/utils/randomCity';
-
-type WeatherData = {
-  location: {
-    name: string;
-    country: string;
-    region: string;
-    localtime: string;
-  };
-};
+import type { WeatherData } from '@/types/propTypes';
 
 const Hero = () => {
   // This component fetches weather data for a random city when the button is clicked
   const [loading, setLoading] = useState(false);
   const [info, setInfo] = useState<WeatherData | null>(null);
-  const [country] = useState(info?.location?.country || 'Tssss');
-  //   const region = info?.location?.region;
-  //   const localTime = info?.location?.localtime;
-  const [cityName] = useState(info?.location?.name);
 
+  const { country, name } = info?.location || {
+    country: 'Tssss',
+    name: 'Tssss',
+  };
+
+  // function for fetching weather data for a random city
   const fetchWeatherData = async () => {
     try {
       setLoading(true);
       const data = await fetcher(`${API_URL}&q=${randomCity()}&aqi=no`);
       if (!data.length) {
-        console.log(data);
-        setInfo(data[0]);
+        console.log('Data:', data);
+        setInfo(data);
       }
     } catch (error) {
       console.error('Error fetching weather data:', error);
@@ -41,12 +35,19 @@ const Hero = () => {
     <section className="container flex flex-col items-center justify-center h-screen">
       <div className="prose bg-gray-100 rounded-md p-4">
         <h1>
-          BONJOUR {cityName} - {country}
+          BONJOUR <span className="text-green-600">{name}</span> -{' '}
+          <span className="text-gray-700">{country}</span>
         </h1>
-        <p>Hello! My name is Jump Jump to quick haha...</p>
+        <p className="inline mr-2">{info?.current?.condition?.text}</p>
+        <span className="inline-block mr-2">
+          <b>- {info?.current?.temp_c}°C</b>
+        </span>
+        <span>
+          <b>{info?.current?.temp_f}°F</b>
+        </span>
         <Button
           onClick={fetchWeatherData}
-          className={`bg-blue-700 text-white
+          className={`bg-blue-700 text-white mt-2
             hover:bg-blue-800 focus:bg-blue-900
             focus:outline-none focus:ring-2 focus:ring-blue-500
             focus:ring-offset-2 transition-all duration-300 ease-in-out flex justify-center items-center ${
