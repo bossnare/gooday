63a0754729f759ad9ee553ce6b8252420b81e82b
bossnare <razafimangagervaischristo@gmail.com>
2025-07-06 20:34:03 +0300

    ref: refactor Hero component to improve weather data handling and update propTypes for WeatherData

--- src/types/propTypes.ts
+++ src/types/propTypes.ts
@@ -5,4 +5,27 @@ type ButtonProps = {
   loading?: boolean;
 };
 
-export type { ButtonProps };
+type WeatherData = {
+  location: {
+    name: string;
+    country: string;
+    region: string;
+    localtime: string;
+  };
+  current: {
+    temp_c: number;
+    temp_f: number;
+    condition: {
+      text: string;
+      icon: string;
+    };
+    wind_kph: number;
+    humidity: number;
+  };
+  error?: {
+    code: number;
+    message: string;
+  };
+};
+
+export type { ButtonProps, WeatherData };
