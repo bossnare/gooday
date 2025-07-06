7d39f60a9542e1115c4c42f701fe0525b5a54e90
bossnare <razafimangagervaischristo@gmail.com>
2025-07-06 17:18:58 +0300

    feat: enhance Hero component to fetch weather data and update Button component to support loading state

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,9 +1,10 @@
 import type { ButtonProps } from '@/types/propTypes';
 
-const Button = ({ label, className, onClick }: ButtonProps) => {
+const Button = ({ label, className, onClick, loading }: ButtonProps) => {
   return (
     <button onClick={onClick} className={`py-1.5 px-4 rounded-md ${className}`}>
-      {label}
+      {loading && <span className="animate-spin">🔄</span>}
+      {!loading && label}
     </button>
   );
 };
