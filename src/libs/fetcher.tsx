01bbf25bb60b0d20390a092cb6377b21e99a8b62
bossnare <razafimangagervaischristo@gmail.com>
2025-07-06 15:10:29 +0300

    tools: add swr

--- src/libs/fetcher.tsx
+++ src/libs/fetcher.tsx
@@ -1,12 +1,7 @@
 import type { FetchProps } from '@/types/propTypes';
 
 export const fetcher = async ({ url }: FetchProps) => {
-  try {
     const res = await fetch(url);
     const data = await res.json();
     return data;
-  } catch (err: unknown) {
-    console.log(err);
-    return err
-  }
 };
