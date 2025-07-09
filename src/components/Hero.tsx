2762e391c80b4b7b4b07c588c3417b59fc111ec0
bossnare <razafimangagervaischristo@gmail.com>
2025-07-09 17:34:52 +0300

    feat: add simple loading fallback

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,40 +1,42 @@
-import { fetcher } from '@/libs/fetcher';
+import useFetch from '@/hooks/useFetch';
 import { API_URL } from '@/utils/constants';
-import Button from './ui/Button';
-import { useState } from 'react';
 import randomCity from '@/utils/randomCity';
-import type { WeatherData } from '@/types/data/api';
 import { roundFormat } from '@/utils/roundFormat';
 import { Search } from 'lucide-react';
 import { Test } from './Test';
+import Button from './ui/Button';
 
 const Hero = () => {
   // This component fetches weather data for a random city when the button is clicked
-  const [loading, setLoading] = useState(false);
-  const [info, setInfo] = useState<WeatherData | null>(null);
+  // const [loading, setLoading] = useState(false);
+  // const [info, setInfo] = useState<WeatherData | null>(null);
+  const { data, error, isLoading } = useFetch(
+    `${API_URL}&q=${randomCity() || 'Antananarivo'}&aqi=no`
+  );
 
-  const { country, name } = info?.location || {
-    country: 'None',
-    name: 'None',
+  const { country, name } = data?.location || {
+    country: '',
+    name: '',
   };
 
   // function for fetching weather data for a random city
-  const fetchWeatherData = async () => {
-    try {
-      setLoading(true);
-      const data = await fetcher(
-        `${API_URL}&q=${randomCity() || 'Antananarivo'}&aqi=no`
-      );
-      if (!data.length) {
-        console.log('Data:', data);
-        setInfo(data);
-      }
-    } catch (error) {
-      console.error('Error fetching weather data:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const fetchWeatherData = async () => {
+
+  // try {
+  //   setLoading(true);
+  //   const data = await fetcher(
+  //     `${API_URL}&q=${randomCity() || 'Antananarivo'}&aqi=no`
+  //   );
+  //   if (!data.length) {
+  //     console.log('Data:', data);
+  //     setInfo(data);
+  //   }
+  // } catch (error) {
+  //   console.error('Error fetching weather data:', error);
+  // } finally {
+  //   setLoading(false);
+  // }
+  // };
 
   return (
     <section className=" mx-auto w-full flex flex-col gap-10 items-center justify-center h-screen">
@@ -55,27 +57,34 @@ const Hero = () => {
           </button>
         </div>
 
-        <h1>
-          <span className="text-purple-500">{name}</span> -{' '}
-          <span className="text-slate-400">{country}</span>
-        </h1>
-        <p className="inline mr-2">{info?.current?.condition?.text}</p>
-        <span className="inline-block mr-2">
-          <b>~ {roundFormat(info?.current?.temp_c || 0)}°C</b>
-        </span>
-        <span>
-          <b>~ {roundFormat(info?.current?.temp_f || 0)}°F</b>
-        </span>
+        {isLoading ? (
+          <p>Loading ...</p>
+        ) : (
+          <>
+            <h1>
+              <span className="text-purple-500">{name}</span> -{' '}
+              <span className="text-slate-400">{country}</span>
+            </h1>
+            <p className="inline mr-2">{data?.current?.condition?.text}</p>
+            <span className="inline-block mr-2">
+              <b>~ {roundFormat(data?.current?.temp_c || 0)}°C</b>
+            </span>
+            <span>
+              <b>~ {roundFormat(data?.current?.temp_f || 0)}°F</b>
+            </span>
+          </>
+        )}
+        {error && <p>{error.name}</p>}
         <Button
-          onClick={fetchWeatherData}
+          // onClick={fetchWeatherData}
           className={`bg-indigo-700 text-white mt-2
             hover:bg-indigo-800 focus:bg-indigo-900
             focus:outline-none focus:ring-2 focus:ring-indigo-500
             focus:ring-offset-2 transition-all duration-300 ease-in-out flex justify-center items-center ${
-              loading ? 'opacity-50 cursor-not-allowed' : ''
+              isLoading ? 'opacity-50 cursor-not-allowed' : ''
             }`}
           label={'Start'}
-          loading={loading}
+          loading={isLoading}
         />
       </div>
 
