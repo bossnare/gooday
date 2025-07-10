d58b18c71ae6e52bd29a6201caa7fc97b16b9a81
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-07-10 16:26:27 +0300

    Update & fix: header to sticky position

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Main from './components/Main';
 function App() {
   return (
     <>
-      <header className="w-full fixed py-4 bg-gray-100">
+      <header className="w-full sticky py-4 bg-gray-100">
         <div className="container font-black text-gray-600">
           <h1>GOODAY</h1>
         </div>
