e01487266e8fc9e1502cd93224fc4919348c9c87
bossnare <razafimangagervaischristo@gmail.com>
2025-07-21 10:19:15 +0300

    UX: big fix of sidebar, Thanks to BossNare

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import type { HistoryData } from '@/types/data/history';
-import { StretchHorizontal, History } from 'lucide-react';
+import { History, StretchHorizontal } from 'lucide-react';
 import { useState } from 'react';
 import HistoryCard from './components/HistoryCard';
 import Main from './components/Main';
@@ -31,7 +31,7 @@ function App() {
         {/* Overlay */}
         <div
           onClick={() => setIsSown(false)}
-          className={`fixed inset-0 z-50 lg:hidden transition-opacity duration-300 ${
+          className={`fixed inset-0 z-5 lg:hidden transition-opacity duration-300 ${
             isSown
               ? ' bg-black/40 opacity-100'
               : 'opacity-0 pointer-events-none'
@@ -39,49 +39,38 @@ function App() {
         ></div>
 
         <aside
-          className={`lg:w-[25%] w-full z-10 border-r-1 border-r-gray-100 lg:sticky top-0 left-0 h-full lg:h-auto fixed`}
+          className={`lg:w-[25%] ${
+            isSown ? 'translate-x-0' : '-translate-x-full px-0'
+          } bg-white w-5/6 lg:translate-x-0 will-change-transform h-full flex flex-col transition-transform duration-400 ease-in-out z-10 border-r-1 border-r-gray-100 lg:sticky top-0 left-0 lg:h-auto fixed`}
         >
-          <div
-            className={`${
-              isSown ? 'translate-x-0' : '-translate-x-full px-0'
-            } bg-white w-5/6 lg:translate-x-0 lg:w-full will-change-transform h-full flex flex-col transition-transform duration-400 ease-in-out`}
-          >
-            <div className="flex items-center px-2 py-1 border-b-1 border-b-gray-100">
-              <img
-                src="/nav_icon_x32.svg"
-                alt="logo"
-                className="w-10 lg:w-10"
-              />
+          <div className="flex items-center px-2 py-1 border-b-1 border-b-gray-100">
+            <img src="/nav_icon_x32.svg" alt="logo" className="w-10 lg:w-10" />
 
-              <button className="ml-auto icon" onClick={() => setIsSown(false)}>
-                <StretchHorizontal />
-              </button>
-            </div>
+            <button className="ml-auto icon" onClick={() => setIsSown(false)}>
+              <StretchHorizontal />
+            </button>
+          </div>
 
-            <div className="px-4 mt-3 mb-5 overflow-y-auto prose prose-gray grow-1 scroll-smooth">
-              <h3 className="flex items-center gap-4 text-gray-500">
-                Search history <History />
-              </h3>
+          <div className="h-full px-4 mt-3 mb-5 overflow-y-auto prose prose-gray grow-1 scroll-smooth">
+            <h3 className="flex items-center gap-4 text-gray-500">
+              Search history <History />
+            </h3>
 
-              {history.length > 0 ? (
-                <ul className="h-auto p-0 rounded-lg *:m-0 pt-1 list-none border-gray-200 divide-y divide-gray-200 border-1">
-                  {history.map((hist) => (
-                    <HistoryCard key={hist.id} {...hist} />
-                  ))}
-                </ul>
-              ) : (
-                <div className="pl-2">
-                  <p className="!text-gray-500">
-                    Nothing search history, please create one. To create this,
-                    go to search bar and search your favourite city.
-                  </p>
-                  <Button
-                    label="Create one"
-                    className="text-white gradient-bg"
-                  />
-                </div>
-              )}
-            </div>
+            {history.length > 0 ? (
+              <ul className="h-auto p-0 rounded-lg *:m-0 pt-1 list-none border-gray-200 divide-y divide-gray-200 border-1">
+                {history.map((hist) => (
+                  <HistoryCard key={hist.id} {...hist} />
+                ))}
+              </ul>
+            ) : (
+              <div className="pl-2">
+                <p className="!text-gray-500">
+                  Nothing search history, please create one. To create this, go
+                  to search bar and search your favourite city.
+                </p>
+                <Button label="Create one" className="text-white gradient-bg" />
+              </div>
+            )}
           </div>
         </aside>
 
